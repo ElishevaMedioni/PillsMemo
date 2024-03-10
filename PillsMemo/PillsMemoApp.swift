02a7395f4eac19ed47fdc81eac268241253a0956
Elish <medioni@g.jct.ac.il>
2024-03-10 19:22:54 +0200

    Initial Commit

---
+++ PillsMemo/PillsMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillsMemoApp.swift
+//  PillsMemo
+//
+//  Created by Elisheva Medioni on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PillsMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
